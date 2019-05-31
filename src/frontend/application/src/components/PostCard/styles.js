4bf9c1404b73148762cfa509550a7236e07bd722
Fredy Álvarez <fre.alvarezb@gmail.com>
2019-05-31 02:16:42 -0500

    Adding post card mock

---
+++ src/frontend/application/src/components/PostCard/styles.js
@@ -0,0 +1,16 @@
+export default {
+  container: {
+    width: "100%",
+    padding: "16px"
+  },
+  content: {
+    marginTop: "5px",
+    marginLeft: "5px"
+  },
+  img: {
+    borderRadius: "50%",
+    display: "block",
+    maxWidth: "100px",
+    maxHeight: "100%"
+  }
+};
