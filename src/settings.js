2f2719451c568b16d05826460f9edd2564cc21c2
Fabricio Ballón Cuadros <fballoncuadros@gmail.com>
2019-06-27 16:03:14 -0500

    port handlign

--- src/settings.js
+++ src/settings.js
@@ -6,6 +6,13 @@ const settingsPath = join(__dirname, '..', 'environment.yml');
 export const settings = safeLoad(readFileSync(settingsPath, 'utf-8'));
 
 settings.host = settings.host ? settings.host : '0.0.0.0';
+if (process.env.$PORT) {
+  settings.port = process.env.$PORT
+} else if (settings.port) {
+  settings.port = settings.port
+} else {
+  settings.port = 8000
+}
 const {
   domain, port, user, password, name,
 } = settings.database;
