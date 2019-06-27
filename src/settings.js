d797d425a643ef4a23f5d4808870b32347945e48
Fabricio Ballón Cuadros <fballoncuadros@gmail.com>
2019-06-27 16:04:57 -0500

    chenged settings

--- src/settings.js
+++ src/settings.js
@@ -7,7 +7,7 @@ export const settings = safeLoad(readFileSync(settingsPath, 'utf-8'));
 
 settings.host = settings.host ? settings.host : '0.0.0.0';
 if (process.env.$PORT) {
-  settings.port = process.env.$PORT
+  settings.port = process.env.PORT
 } else if (settings.port) {
   settings.port = settings.port
 } else {
